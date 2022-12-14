7606208cb36a0843bd1c681df3db8fd6e40139aa
Maksimilian <redmechax00@gmail.com>
2022-12-14 12:15:19 +0300

    Done

--- app/src/main/java/com/redmechax00/astonintensive1/MainActivity.kt
+++ app/src/main/java/com/redmechax00/astonintensive1/MainActivity.kt
@@ -2,10 +2,16 @@ package com.redmechax00.astonintensive1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickMain(v: View) {
+        Toast.makeText(this, getString(R.string.btn_main_click_text), Toast.LENGTH_SHORT).show()
     }
 }
